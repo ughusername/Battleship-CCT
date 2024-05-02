c18eb4211ea722468dc14b42ae903cad09d8d533
ughusername <168083118+ughusername@users.noreply.github.com>
2024-05-02 15:39:58 +0000

    Co-authored-by: Achemd <Achemd@users.noreply.github.com>

--- Computer Science CCT/BattleshipMain.java
+++ Computer Science CCT/BattleshipMain.java
@@ -2,5 +2,6 @@ public class BattleshipMain {
     public static void main(String[] args) {
         System.out.println("CCT");
         System.out.println("Helloworld");
+        System.out.println("umm... what the sigma!!! ");
     }    
 }
